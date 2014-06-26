15a40f3f0c22c51f798b7b49da817e1c343e2123
Nathan LaFreniere <quitlahok@gmail.com>
2014-06-26 14:37:15 -0700

    allow type = counter in get/put

--- index.js
+++ index.js
@@ -49,35 +49,44 @@ RiakDOWN.prototype._close = function (callback) {
 
 RiakDOWN.prototype._put = function (key, value, options, callback) {
     var bucket = options.bucket || this._bucket;
-    var indexes = [].concat(options.indexes || []).map(function (index) {
-        return {
-            key: /_(bin|int)$/.test(index.key) ? index.key : index.key + '_bin',
-            value: index.value
-        };
-    });
 
-    indexes.forEach(function (index) {
-        indexes.push({
-            key: '_reverse_' + index.key,
-            value: /_int$/.test(index.key) ? -1 * index.value : utils.reverseString(index.value)
+    if (options.type === 'counter') {
+        this._client.put({
+            bucket: bucket,
+            key: toKey(key),
+            amount: value
+        }, callback);
+    } else {
+        var indexes = [].concat(options.indexes || []).map(function (index) {
+            return {
+                key: /_(bin|int)$/.test(index.key) ? index.key : index.key + '_bin',
+                value: index.value
+            };
         });
-    });
 
-    indexes.push({
-        key: '_reverse_key_bin',
-        value: utils.reverseString(toKey(key))
-    });
-    
-    this._client.put({
-        bucket: bucket,
-        key: toKey(key),
-        content: {
-            value: value,
-            content_type: options.content_type || 'application/octet-stream',
-            indexes: indexes
-        },
-        vclock: options.vclock
-    }, callback);
+        indexes.forEach(function (index) {
+            indexes.push({
+                key: '_reverse_' + index.key,
+                value: /_int$/.test(index.key) ? -1 * index.value : utils.reverseString(index.value)
+            });
+        });
+
+        indexes.push({
+            key: '_reverse_key_bin',
+            value: utils.reverseString(toKey(key))
+        });
+        
+        this._client.put({
+            bucket: bucket,
+            key: toKey(key),
+            content: {
+                value: value,
+                content_type: options.content_type || 'application/octet-stream',
+                indexes: indexes
+            },
+            vclock: options.vclock
+        }, callback);
+    }
 };
 
 RiakDOWN.prototype._get = function (key, options, callback) {
@@ -85,31 +94,44 @@ RiakDOWN.prototype._get = function (key, options, callback) {
     var bucket = options.bucket || this._bucket;
     var newoptions;
 
-    this._client.get({ bucket: bucket, key: toKey(key) }, function (err, reply) {
-        if (err || (!reply || !reply.content || !reply.content.length)) {
-            return callback(new Error('NotFound'));
-        }
-
-        if (reply.content.length === 1) {
-            return callback(null, options.asBuffer === false ? reply.content[0].value.toString() : reply.content[0].value);
-        }
-
-        newoptions = JSON.parse(JSON.stringify(options)); // ugly clone so we don't modify the original options
-        newoptions.bucket = bucket;
-        RiakDOWN._siblingResolver(key, reply.content, newoptions, function (err, resolved) {
-            if (err) {
-                return callback(err);
+    if (options.type === 'counter') {
+        this._client.getCounter({
+            bucket: bucket,
+            key: toKey(key)
+        }, function (err, reply) {
+            if (err || (!reply)) {
+                return callback(new Error('NotFound'));
             }
 
-            if (typeof resolved !== 'string' && !Buffer.isBuffer(resolved)) {
-                resolved = JSON.stringify(resolved);
+            callback(null, options.asBuffer === false ? reply.value.toString(10) : new Buffer(reply.value.toString(10)));
+        });
+    } else {
+        this._client.get({ bucket: bucket, key: toKey(key) }, function (err, reply) {
+            if (err || (!reply || !reply.content || !reply.content.length)) {
+                return callback(new Error('NotFound'));
             }
 
-            self._put(key, resolved, util._extend(newoptions, { bucket: bucket, vclock: reply.vclock }), function (err) {
-                self._get(key, options, callback);
+            if (reply.content.length === 1) {
+                return callback(null, options.asBuffer === false ? reply.content[0].value.toString() : reply.content[0].value);
+            }
+
+            newoptions = JSON.parse(JSON.stringify(options)); // ugly clone so we don't modify the original options
+            newoptions.bucket = bucket;
+            RiakDOWN._siblingResolver(key, reply.content, newoptions, function (err, resolved) {
+                if (err) {
+                    return callback(err);
+                }
+
+                if (typeof resolved !== 'string' && !Buffer.isBuffer(resolved)) {
+                    resolved = JSON.stringify(resolved);
+                }
+
+                self._put(key, resolved, util._extend(newoptions, { bucket: bucket, vclock: reply.vclock }), function (err) {
+                    self._get(key, options, callback);
+                });
             });
         });
-    });
+    }
 };
 
 RiakDOWN.prototype._del = function (key, options, callback) {
